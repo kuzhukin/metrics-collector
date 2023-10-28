b43af0dd30a290dc9aaf8e7989759c7d4950b25b
kkuzhukin <kkuzhukin@tradingview.com>
2023-10-28 21:26:41 +0300

    Use default chi way for using middlewares

--- internal/server/server.go
+++ internal/server/server.go
@@ -26,18 +26,19 @@ func StartNew(config Config) *MetricServer {
 	updateHandler := handler.NewUpdateHandler(storage, parser.New())
 	valueHandler := handler.NewValueHandler(storage, parser.New())
 
+	router.Use(middleware.LoggingHTTPHandler)
+	router.Use(middleware.CompressingHTTPHandler)
+
 	router.Handle(endpoint.RootEndpoint, listHandler)
 	router.Handle(endpoint.UpdateEndpoint, updateHandler)
 	router.Handle(endpoint.UpdateEndpointJSON, updateHandler)
 	router.Handle(endpoint.ValueEndpoint, valueHandler)
 	router.Handle(endpoint.ValueEndpointJSON, valueHandler)
 
-	handler := addMiddlewares(router)
-
 	server := &MetricServer{
 		srvr: http.Server{
 			Addr:    config.Hostport,
-			Handler: handler,
+			Handler: router,
 		},
 		wait: make(chan struct{}),
 	}
@@ -53,13 +54,6 @@ func StartNew(config Config) *MetricServer {
 	log.Logger.Infof("Server started hostport=%v", config.Hostport)
 
 	return server
-}
-
-func addMiddlewares(handler http.Handler) http.Handler {
-	handler = middleware.CompressingHTTPHandler(handler)
-	handler = middleware.LoggingHTTPHandler(handler)
-
-	return handler
 }
 
 func (s *MetricServer) Stop() error {
